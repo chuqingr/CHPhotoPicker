f94f7519be7edcec3ad9cf0379ed691c379d1ad8
chuqingr <953553352@qq.com>
2016-06-08 17:16:53 +0800

    Initial Commit

---
+++ CHPhotoPicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CHPhotoPicker
+//
+//  Created by 杨胜浩 on 16/6/8.
+//  Copyright © 2016年 chuqingr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
